f1a3081de8fd0ebc5ce4c076a88ecd34d605b9c1
The Guru <cyberreefguru@gmail.com>
2025-06-15 10:28:30 -0400

    Added custom seesaw libraries

--- src/relay/relay_module.cpp
+++ src/relay/relay_module.cpp
@@ -1,20 +1,18 @@
-#define PRODUCT_CODE            1234
-#define CONFIG_I2C_PERIPH_ADDR  0x48
-//#define CONFIG_UART_DEBUG          1
-
-#define CONFIG_INTERRUPT_PIN      12
-
-// Can have up to 3 addresses
-#define CONFIG_ADDR_0_PIN          6
-#define CONFIG_ADDR_1_PIN          7
+#include "ModuleHardware.h"
+#define PRODUCT_CODE            MODULE_RELAY_PRODUCT_CODE
+#define CONFIG_I2C_PERIPH_ADDR  MODULE_RELAY_ADDRESS
+#define CONFIG_CHANNELS 1
 
 #include <Adafruit_seesawPeripheral.h>
+
 
 void setup() {
 #if CONFIG_UART_DEBUG
   Serial.begin(115200);
   delay(500);
 #endif
+
+  setChannelCount(1);
 
   Adafruit_seesawPeripheral_begin();
 }
