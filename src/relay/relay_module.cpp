fda3cb283aa5a47763b778ab21add8238b820970
The Guru <cyberreefguru@gmail.com>
2025-06-20 22:11:51 -0400

    Testing optmizations

--- src/relay/relay_module.cpp
+++ src/relay/relay_module.cpp
@@ -1,7 +1,12 @@
 #include "ModuleHardware.h"
+
 #define PRODUCT_CODE            MODULE_RELAY_PRODUCT_CODE
 #define CONFIG_I2C_PERIPH_ADDR  MODULE_RELAY_ADDRESS
 #define CONFIG_CHANNELS 1
+
+// #define CONFIG_INTERRUPT_PIN 8
+// #define CONFIG_INTERRUPT 1
+// #define USE_PINCHANGE_INTERRUPT 1
 
 #include <Adafruit_seesawPeripheral.h>
 
